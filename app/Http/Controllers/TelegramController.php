36784e78e7881b4cae54a082f39b696042545228
Zaevdev <zaevdev@gmail.com>
2022-05-22 20:26:22 +0300

    Delete useless logger in Controller

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -8,24 +8,20 @@ use App\Http\Services\Client\TelegramClient;
 use App\Http\Services\Telegram\TelegramService;
 use Illuminate\Http\Request;
 use JsonException;
-use Psr\Log\LoggerInterface;
 
 class TelegramController extends Controller
 {
-    public LoggerInterface $logger;
 
     public TelegramService $service;
 
     public TelegramClient $client;
 
     /**
-     * @param LoggerInterface $logger
      * @param TelegramService $service
      * @param TelegramClient $client
      */
-    public function __construct(LoggerInterface $logger, TelegramService $service, TelegramClient $client)
+    public function __construct(TelegramService $service, TelegramClient $client)
     {
-        $this->logger = $logger;
         $this->service = $service;
         $this->client = $client;
     }
